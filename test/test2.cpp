c2adc31658e513ecb133fa2cf57b2ff813206f69
mithun218 <bairagirasulpur@gmail.com>
2022-06-11 17:06:30 +0530

    add some tests

--- test/test2.cpp
+++ test/test2.cpp
@@ -4,7 +4,9 @@
  *   Program to test GiNaCDE library. This program solve following pdes
         i.  Cahn-Allen equation
         ii. Duffing equation
-        iii.Nonlinear Telegraph Equation      */
+        iii.Nonlinear Telegraph Equation 
+        iv. Kudryashov-Sinelshchikov  equation 
+        v.  Benjamin-Bona-Mahony equation    */
 
 
 
@@ -106,6 +108,64 @@ int main()
          }
 
      }
+
+
+   pde = Diff(u,t,1)+u*Diff(u,x,1)+Diff(u,x,3)-Diff(u*Diff(u,x,2),x,1)-Diff(u,x,1)*Diff(u,x,2)-Diff(u,x,2)-Diff(u*Diff(u,x,1),x,1);  // Kudryashov-Sinelshchikov  equation
+    output = ginac;
+    twcPhase=lst{lst{k_0,k_1},lst{}};
+    degAcoeff = lst{2,0,A_1,A_2};
+    NValue = 2;
+    filename = "Kudryashov-Sinelshchikov_mF.txt";
+    desolve(pde, {u}, mF_expansion,true);
+    /* Checking all solutions*/
+     diffStr.str("");
+     diffStr<<pde;
+     solu_num1 = (solutionClt).size();
+     for(size_t i=0;i<solu_num1;i++)
+     {
+         algSoluStr.str("");
+         algSoluStr<<solutionClt[i][0];
+         solu_num2 = nops(solutionClt[i]);
+         for(size_t j=1;j<solu_num2;j++)
+         {
+             diffSoluStr.str("");
+             diffSoluStr<<solutionClt[i][j];
+             res = checkSolu(diffStr.str(),diffSoluStr.str(),algSoluStr.str());
+             if(res!=_ex0)
+                 return -1;
+         }
+
+     }
+
+
+    pde = Diff(u,t,1)+Diff(u,x,1)+u*Diff(u,x,1)-Diff(Diff(u,x,2),t,1);  // Benjamin-Bona-Mahony equation
+    output = ginac;
+    twcPhase=lst{lst{k_0,k_1},lst{}};
+    degAcoeff = lst{4,0,0,A_2,0,A_4};
+    ASolve = true;
+    positivePart = true;
+    negativePart = false;
+    filename = "benjamin_Fex.txt";
+    desolve(pde, {u}, F_expansion,true);
+    /* Checking all solutions*/
+     diffStr.str("");
+     diffStr<<pde;
+     solu_num1 = (solutionClt).size();
+     for(size_t i=0;i<solu_num1;i++)
+     {
+         algSoluStr.str("");
+         algSoluStr<<solutionClt[i][0];
+         solu_num2 = nops(solutionClt[i]);
+         for(size_t j=1;j<solu_num2;j++)
+         {
+             diffSoluStr.str("");
+             diffSoluStr<<solutionClt[i][j];
+             res = checkSolu(diffStr.str(),diffSoluStr.str(),algSoluStr.str());
+             if(res!=_ex0)
+                 return -1;
+         }
+
+     }          
 
     return 0;
 
